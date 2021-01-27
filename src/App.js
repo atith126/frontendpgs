c39c5cd17a71d33d2ffb0b7943b2e8e5bb3cfbd6
atith126 <atithrokani@gmail.com>
2021-01-27 13:20:26 +0530

    Your commit message 2

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ function App() {
 			  <Navbar.Toggle aria-controls="basic-navbar-nav" />
 			  <Navbar.Collapse id="basic-navbar-nav">
 				  <Nav className="mr-auto">
-					  <Nav.Link href="/">Home</Nav.Link>
-					  <Nav.Link href="/MainContainer">Link</Nav.Link>
+					  <Nav.Link href="/frontendpgs">Home</Nav.Link>
+					  <Nav.Link href="/frontendpgs/MainContainer">Link</Nav.Link>
 				  </Nav>
 			  </Navbar.Collapse>
 		  </Navbar>
